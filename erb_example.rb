12b41577d36509cde258af0c937fabf64dea61d7
Alejandro Corredor <Alejandro@Alejandros-MacBook-Air.local>
2015-03-02 01:30:04 -0500

    homework section ERB completed

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,17 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+x = x - 20
+template = ERB.new "I changed the value of x to <%= x %>"
+puts template.result(binding)
+
+myName = "Alejandro"
+momName = "Thais"
+myHash = {"Alejandro" => "Corredor", "Thais" => "Lopez"}
+template = ERB.new "My last name is <%= myHash['#{myName}'] %> and my
+mom's last name is <%= myHash['#{momName}'] %>"
+puts template.result(binding)
+
+template = ERB.new "<%= 5 + 5 + 5 * 20 %>"
+puts template.result(binding)
